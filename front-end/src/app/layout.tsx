286a91d16dcfe3266930eb3948f850dc5ef8b9cf
MatheusLRocha <matheuslrocha10@gmail.com>
2026-08-23 15:38:53 -0300

    Pagina inicial

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "./globals.css";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: 'Library Home'
+}
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      className="h-full"
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
