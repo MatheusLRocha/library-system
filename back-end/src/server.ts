d7a097df39aee6a90f9e9ff901cbeb12774760d0
MatheusLRocha <matheuslrocha10@gmail.com>
2026-08-24 02:23:28 -0300

    Adição do metodo get e post

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -8,8 +8,26 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
+interface User {
+    id: string,
+    name: string,
+    ra: string,
+    email: string,
+}
+
+app.get('/api/users', (req, res) => {
     res.send('Users');
+});
+
+app.post('/api/users', (req, res) => {
+    const newUser: User = {
+        id: '1',
+        name: 'Matheus',
+        ra: '111111',
+        email: 'emailtest@gmail.com',
+    };
+
+    res.json(newUser);
 });
 
 app.listen(PORT, () => console.log(`Server connected: http://localhost:${PORT}`));
