002896df488a30963b47b464dabe723a9f14840f
MatheusLRocha <matheuslrocha10@gmail.com>
2026-08-24 01:56:45 -0300

    Criação da base

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,1 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    res.send('Users');
+});
+
+app.listen(PORT, () => console.log(`Server connected: http://localhost:${PORT}`));
