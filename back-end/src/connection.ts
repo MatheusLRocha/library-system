9c9940b31b92171a749fa9aa5940742c5d555181
MatheusLRocha <matheuslrocha10@gmail.com>
2026-08-26 02:24:14 -0300

    <feat>: Migrations criada

--- back-end/src/connection.ts
+++ back-end/src/connection.ts
@@ -1,9 +1,31 @@
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../database/database.sqlite'
+    storage: '../database/production.sqlite'
 });
+
+const User = sequelize.define(
+    'User',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ra: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        }
+    }
+)
+
+
 
 try {
     await sequelize.authenticate();
