a971f924c38965204273b5142bb3bd4567c101e4
MatheusLRocha <matheuslrocha10@gmail.com>
2026-08-31 15:42:41 -0300

    <feat>: Tela do usuário

--- front-end/src/components/UserMenu.tsx
+++ front-end/src/components/UserMenu.tsx
@@ -1,14 +1,19 @@
+import { User } from "../api";
 
-interface Props {
-    title: string,
+type Props = {
+    user: User,
 }
 
-export default function UserMenu({ title }: Props) {
+export default function UserMenu({ user }: Props) {
     return (
-        <div className="flex bg-blue-500/30 w-md rounded-md border-2 border-blue-600 justify-center relative">
+        <div className="flex flex-col bg-blue-500/30 w-md rounded-md border-2 border-blue-600 items-center gap-20 relative">
             
-            <h1 className="text-2xl text-white font-bold">{ title }</h1>
+            <h1 className="text-3xl text-white font-bold mt-5">{ user.name }</h1>
             
+            <div className="flex flex-col gap-2">
+                <h3 className="text-white"><strong>RA:</strong> {user.ra}</h3>
+                <h3 className="text-white"><strong>Email:</strong> {user.email}</h3>
+            </div>
         </div>
     )
 }
